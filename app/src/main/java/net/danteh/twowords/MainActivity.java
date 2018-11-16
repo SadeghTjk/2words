7bb7f973b77baec91df03a7c7b06558d872eb6cf
Sadegh <prince.tajik@gmail.com>
2018-11-16 11:51:57 +0330

    Revert "Added json reader methods"
    This reverts commit a9c226ada5c7f064509d4520d484d232eef7bd48.

--- app/src/main/java/net/danteh/twowords/MainActivity.java
+++ app/src/main/java/net/danteh/twowords/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
@@ -22,32 +21,47 @@ import java.util.List;
 import co.ronash.pushe.Pushe;
 
 public class MainActivity extends AppCompatActivity {
-    TextView word1, word2;
-    SharedPreferences save = getSharedPreferences("2words", MODE_PRIVATE);
+    TextView word1,word2;
+    SharedPreferences save = getSharedPreferences("2words",MODE_PRIVATE);
     List<Words> wordsList = new ArrayList<>();
-    boolean tempBoolean = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Pushe.initialize(this, true);
+        Pushe.initialize(this,true);
         word1 = findViewById(R.id.word1);
         word2 = findViewById(R.id.word2);
 
         MyPushListener push = new MyPushListener();
 
-        getJson();
-        getWords();
+
+
     }
-    
-    public boolean getJson() {
+
+    public List<Words> readjson(){
         String url = "http://danteh.net/DanTehdes/words.json";
+
         StringRequest stringRequest = new StringRequest(url, new com.android.volley.Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                save.edit().putString("json", response);
-                tempBoolean = true;
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray jsonArray = jsonObject.getJSONArray("data");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+
+                        JSONObject jo = jsonArray.getJSONObject(i);
+                        String english = jo.getString("english");
+                        String id = jo.getString("id");
+                        String persian = jo.getString("persian");
+
+                        Words mywords = new Words(english,id,persian);
+                        wordsList.add(mywords);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }, new com.android.volley.Response.ErrorListener() {
             @Override
@@ -57,35 +71,5 @@ public class MainActivity extends AppCompatActivity {
         });
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         requestQueue.add(stringRequest);
-        return tempBoolean;
-    }
-
-    public void getWords() {
-        try {
-
-            if (!save.getString("json", "").equals("")) {
-                //if sp is not null
-
-                JSONObject jsonObject = new JSONObject(save.getString("json", ""));
-
-                JSONArray jsonArray = jsonObject.getJSONArray("data");
-                for (int i = 0; i < jsonArray.length(); i++) {
-
-                    JSONObject jo = jsonArray.getJSONObject(i);
-                    String english = jo.getString("english");
-                    String id = jo.getString("id");
-                    String persian = jo.getString("persian");
-
-                    Words mywords = new Words(english, id, persian);
-                    wordsList.add(mywords);
-                }
-            }
-            else {
-                Toast.makeText(this, "Connect to internet.", Toast.LENGTH_SHORT).show();
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
     }
 }
